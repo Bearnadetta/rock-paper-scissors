b2e194c4a5475dee48e5d60ea61b526e545c299d
Bearnadetta <bearnadetta@gmail.com>
2021-08-01 10:37:27 -0700

    adding pseudo-code for js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+/* Pseudo-code for Rock Paper Scissors Game:
+When one of the three option blocks is pressed, run the following:
+    playerChoice = block, opponentChoice = random 1/3 chance (assigned to block)
+    display result in results block
+    if playerChoice wins in RPS, increment scoreboard for player. If opponentChoice wins in RPS, increment opponent scoreboard.
+
+    check to see if either player has reached 5 points, if so alert "you win/lose" and reset the game
+    else, reset game
+*/
 alert ('Hey your script sheet is properly linked nice job');
